00d0f4221264efa3b79253a6e9fca89caeaad26b
YoongHM <yoonghm@users.noreply.github.com>
2017-03-13 14:55:54 +0800

    Update rgb.ino

--- rgb/rgb.ino
+++ rgb/rgb.ino
@@ -1,5 +1,3 @@
-// http://tiny.cc/rgb123
-
 #include <math.h>
  
 #ifndef M_PI
@@ -7,20 +5,12 @@
 #endif
 
 const int redPin   =  3;
-const int greenPin =  4;
-const int bluePin  =  5;
-const int sw1      =  2;
-const int ledPin   = 13;
+const int greenPin  =  4;
+const int bluePin =  5;
 
 float f = 0.0;
-volatile int status;
 
 int redPulse, bluePulse, greenPulse;
-
-void trigger() {
-  digitalWrite(ledPin, status);
-  status = !status;
-}
 
 void lit() {
   analogWrite(redPin,   redPulse);
@@ -32,17 +22,13 @@ void setup() {
   pinMode(redPin,   OUTPUT);
   pinMode(bluePin,  OUTPUT);
   pinMode(greenPin, OUTPUT);
-  pinMode(ledPin,   OUTPUT);  
-  pinMode(sw1,      INPUT_PULLUP);
-  status = 1;
-  attachInterrupt(sw1, trigger, FALLING);  
 }
 
 void loop() {
   // Add 1.0 to ensure all values are positive
-  redPulse   = 127 * (sin( (f        ) * M_PI / 180.0) + 1.0);
-  bluePulse  = 127 * (sin( (f + 180.0) * M_PI / 180.0) + 1.0);
-  greenPulse = 127 * (sin( (f + 270.0) * M_PI / 180.0) + 1.0);
+  redPulse   = sin((f      ) * M_PI/180.0) + 1.0;
+  bluePulse  = sin((f+180.0) * M_PI/180.0) + 1.0;
+  greenPulse = sin((f+270.0) * M_PI/180.0) + 1.0;
   lit();
   f += 1.0;
   delay(1);
