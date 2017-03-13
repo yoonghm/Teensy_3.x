c2c234483da6999ebba333ec7af58a476f4570d7
YoongHM <yoonghm@users.noreply.github.com>
2017-03-13 14:36:35 +0800

    Create gpio_interrupt.ino

---
+++ gpio_interrupt/gpio_interrupt.ino
@@ -0,0 +1,48 @@
+// http://tiny.cc/rgb123
+
+#include <math.h>
+ 
+#ifndef M_PI
+#define M_PI 3.1415
+#endif
+
+const int redPin   =  3;
+const int greenPin =  4;
+const int bluePin  =  5;
+const int sw1      =  2;
+const int ledPin   = 13;
+
+float f = 0.0;
+
+int redPulse, bluePulse, greenPulse;
+
+void trigger() {
+  digitalWrite(ledPin, HIGH);
+  delay(10);
+  digitalWrite(ledPin, LOW);
+}
+
+void lit() {
+  analogWrite(redPin,   redPulse);
+  analogWrite(bluePin,  bluePulse);
+  analogWrite(greenPin, greenPulse);
+}
+
+void setup() {
+  pinMode(redPin,   OUTPUT);
+  pinMode(bluePin,  OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(ledPin,   OUTPUT);  
+  pinMode(sw1,      INPUT_PULLUP);
+  attachInterrupt(sw1, trigger, FALLING);  
+}
+
+void loop() {
+  // Add 1.0 to ensure all values are positive
+  redPulse   = sin( (f        ) * M_PI / 180.0) + 1.0;
+  bluePulse  = sin( (f + 180.0) * M_PI / 180.0) + 1.0;
+  greenPulse = sin( (f + 270.0) * M_PI / 180.0) + 1.0;
+  lit();
+  f += 1.0;
+  delay(1);
+}
