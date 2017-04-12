d4a2ed66924184a82031ba9e52e3d0609188d2b8
YoongHM <yoonghm@users.noreply.github.com>
2017-04-13 02:46:17 +0800

    Working version

--- OLED_SSD1351/OLED_SSD1351.ino
+++ OLED_SSD1351/OLED_SSD1351.ino
@@ -1,7 +1,5 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include <ssd1351.h>
 #include <TimeLib.h>
+#include <ssd1351.h>
 
 typedef ssd1351::HighColor Color;
 const int size_x = 128;
@@ -9,10 +7,6 @@ const int size_y = 128;
 // auto display = ssd1351::SSD1351<Color, ssd1351::NoBuffer, size_x, size_y>();
 auto display = ssd1351::SSD1351<Color, ssd1351::SingleBuffer, size_x, size_y>();
 
-volatile int timer_1s = 0;
-volatile int status = 0;
-
-// weekday() returns integer: 1 = Sunday
 const char *WeekdayName[] = {
   "      "   ,   // 0
   "Sunday"   ,   // 1
@@ -24,7 +18,6 @@ const char *WeekdayName[] = {
   "Saturday"     // 7
 };
 
-// month() return integer: 1 = January
 const char *MonthName[] = {
   "   "     ,    // 0
   "Jan"     ,    // 1
@@ -41,63 +34,58 @@ const char *MonthName[] = {
   "Dec"          // 12
 };
 
-void systick_isr() {
-  systick_millis_count++;   // Original ISR code
-  if (systick_millis_count % 1000 == 0) {
-    timer_1s = 1;
-  }
-}
-
 time_t getTeensy3Clock() {
   return Teensy3Clock.get();
 }
 
-void printDate()
+void displayDigitalClock()
 {
+  char str[12];
+  uint16_t w;
+
   display.setFont(FreeSans9pt7b);
   display.setTextSize(1);
   display.setTextColor(ssd1351::RGB(255, 255, 255));
-
-  char str[12];
   snprintf(str, 12, "%02d-%s-%d", day(), MonthName[month()], year());
-#if 0
-  snprintf(datetime, DATETIMELEN,
-           "%02d-%s-%d %s %02d:%02d:%02d",
-           day(), MonthName[month()], year(), WeekdayName[weekday()],
-           hour(), minute(), second()
-          );
-#endif
-  uint16_t w = display.getTextWidth(str);
+  w = display.getTextWidth(str);
   display.setCursor((size_x - w)/2, 20);
+  display.print(str);
+
+  display.setFont(FreeSansBold12pt7b);
+  display.setTextSize(1);
+  display.setTextColor(ssd1351::RGB(0, 64, 255));
+  snprintf(str, 9, "%02d:%02d:%02d", hour(), minute(), second());
+  display.setCursor(20, 45);
+  display.print(str);
+
+  display.setFont(FreeSans12pt7b);
+  display.setTextSize(1);
+  display.setTextColor(ssd1351::RGB(255, 255, 255));
+  snprintf(str, 9, "%s", WeekdayName[weekday()]);
+  w = display.getTextWidth(str);
+  display.setCursor((size_x - w)/2, 70);
   display.print(str);
 }
 
 void setup() {
   Serial.println("Hi");
 
+  setSyncProvider(getTeensy3Clock);
+
   display.begin();
   display.fillScreen(ssd1351::RGB());
 
-  setSyncProvider(getTeensy3Clock);
-  printDate();
-
-//display.drawPixel(0, 0, ssd1351::RGB(255,255,255));
-//display.drawPixel(size_x/2, size_y/2, ssd1351::RGB(255,255,255));
-//display.drawPixel(size_x-1, size_y-1, ssd1351::RGB(255,255,255));
+#if 0
+  display.drawPixel(0, 0, ssd1351::RGB(255,255,255));
+  display.drawPixel(size_x/2, size_y/2, ssd1351::RGB(255,255,255));
+  display.drawPixel(size_x-1, size_y-1, ssd1351::RGB(255,255,255));
+#endif
 
   display.updateScreen();
-  display.end();
 }
 
 void loop() {
-
-  if (timer_1s == 1)
-  {
-    timer_1s = 0;
-    display.begin();
-    printDate();
-    display.updateScreen();
-    display.end();
-    status = !status;
-  }
+  display.fillScreen(ssd1351::RGB());
+  displayDigitalClock();
+  display.updateScreen();
 }
