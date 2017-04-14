c2b52b476c4394afef949cedaf507e2e56380282
yoonghm <YoongHM@gmail.com>
2017-04-14 13:47:57 +0800

    Use digitalReadFast and for loop for delay

--- blink_led/blink_led.ino
+++ blink_led/blink_led.ino
@@ -1,12 +1,11 @@
 void blink(uint8_t pin, uint32_t count) {
-  // volatile uint32_t index;
+  volatile uint32_t index;
 
   for (; count; --count) {
-    digitalWriteFast(pin, !digitalRead(pin));
-    delay(100);
-    digitalWriteFast(pin, !digitalRead(pin));
-    delay(100);
-    // for (index = 0; index < 80000; ++index) {}
+    digitalWriteFast(pin, !digitalReadFast(pin));
+    for (index = 0; index < 80000; ++index) {}
+    digitalWriteFast(pin, !digitalReadFast(pin));
+    for (index = 0; index < 80000; ++index) {}
   }
   Serial.println();
 }
